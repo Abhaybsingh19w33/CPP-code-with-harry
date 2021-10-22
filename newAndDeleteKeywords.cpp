174726dd84132236aa3feb48ed744a50884ccf0b
Abhay Singh <abhaybsingh19w33@gmail.com>
2021-10-22 09:19:29 +0530

    L 50

---
+++ newAndDeleteKeywords.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Basic Example
+    int a = 4;
+    int *ptr = &a;
+    cout << "The value of a is " << *(ptr) << endl;
+
+    float *p = new float(40.78);
+    cout << "The value at address p is " << *(p) << endl;
+
+    int *arr = new int[3];
+    arr[0] = 10;
+    arr[1] = 20;
+    arr[2] = 30;
+    arr[3] = 40;
+    cout << "The value of arr[0] is " << arr[0] << endl;
+    cout << "The value of arr[1] is " << arr[1] << endl;
+    cout << "The value of arr[2] is " << arr[2] << endl;
+    cout << "The value of arr[3] is " << arr[3] << endl;
+
+    cout << "after delete" << endl;
+    delete[] arr;
+    // delete arr;
+    // don't know why only first 2 values are deleted, but remaining ones remains
+    cout << "The value of arr[0] is " << arr[0] << endl;
+    cout << "The value of arr[1] is " << arr[1] << endl;
+    cout << "The value of arr[2] is " << arr[2] << endl;
+    cout << "The value of arr[3] is " << arr[3] << endl;
+    return 0;
+}
